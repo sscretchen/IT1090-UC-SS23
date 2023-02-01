faba6e29d49647cfdbcb5ad961aef421cf6fa818
sscretchen <sjscretchen@gmail.com>
2023-01-31 21:24:54 -0500

    lab 4

---
+++ mod4/Lab_04_Java_Variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int familyKidCount = 5;
+        boolean isRaining = true;
+        double gasByGallon = 3.29;
+        int myFavNumber = 3;
+        double myShoeSize = 12.5;
+        int birthMonth = 1;
+        String fullName = "Sean James Scretchen";
+
+        System.out.println("My name is " + fullName + ". I was born in the month " + birthMonth + " of the year. My favorite number is " + myFavNumber);
+        System.out.println("Did it rain today? " + isRaining);
+        System.out.println("There are " + familyKidCount + " kids in my family");
+        System.out.println("I filled up on gas today. My total cost was " + gasByGallon);
+        System.out.println("My favorite shoes came out this weekend but they were out of size " + myShoeSize);
+    }
+}
